1e2e37e931f3f182e16d27dae223e3600a9975b8
01XASSASSIN <hariharanuv7@gmail.com>
2024-05-12 18:17:43 +0530

    setting up server and db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import "dotenv/config";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -13,6 +16,14 @@ app.get("/", (req, res) => {
   res.json({ message: "Todo Application" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Apllication is running on ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGODBDB_URL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`Application is running at Port:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
